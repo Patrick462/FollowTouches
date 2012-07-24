02d30c7528ebab4f8e63e496a5e6606ec460c6d6
Patrick Weigel <patrick462@aol.com>
2012-07-23 20:52:51 -0500

    Initial Commit

---
+++ ViewMove2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewMove2
+//
+//  Created by Patrick Weigel on 7/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
