7a23c6a01a3af15fd8538f72bdb4afd15f0102dd
Patrick Weigel <patrick462@aol.com>
2012-11-25 13:40:53 -0600

    1. Make height and width of view box (slightly) random. 2. Eliminate unneeded setting of newBox's background color (as the newBox image takes precedence, which is what we want).

--- ViewMove2/ViewController.m
+++ ViewMove2/ViewController.m
@@ -35,34 +35,30 @@
 -(void)addABox:(id)sender
 {
     UIImageView *newBox = [UIImageView new];
-//    UIImageView *happy = [UIImageView new];
     u_int32_t xRandom = arc4random_uniform(668);
     u_int32_t yRandom = 120 + arc4random_uniform(734);
-    newBox.frame = CGRectMake(xRandom, yRandom, 100, 150);
+    u_int32_t widthRandom = arc4random_uniform(20);
+    u_int32_t heightRandom = arc4random_uniform(30);
+    newBox.frame = CGRectMake(xRandom, yRandom, 90 + widthRandom, 135 + heightRandom);
+    
     switch (arc4random_uniform(6))
     {
         case 0:
-            newBox.backgroundColor = [UIColor blueColor];
             newBox.image = [UIImage imageNamed:@"Linen Green 480x480.png"];
             break;
         case 1:
-            newBox.backgroundColor = [UIColor redColor];
             newBox.image = [UIImage imageNamed:@"Linen Grey 480x480.png"];
             break;
         case 2:
-            newBox.backgroundColor = [UIColor brownColor];
             newBox.image = [UIImage imageNamed:@"bnr_hat_only.png"];
             break;
         case 3:
-            newBox.backgroundColor = [UIColor grayColor];
             newBox.image = [UIImage imageNamed:@"background.png"];
             break;
         case 4:
-            newBox.backgroundColor = [UIColor greenColor];
             newBox.image = [UIImage imageNamed:@"background copy.png"];
             break;
         case 5:
-            newBox.backgroundColor = [UIColor greenColor];
             newBox.image = [UIImage imageNamed:@"bomb.png"];
             break;
             
